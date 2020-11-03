d55ecb6619ffff76d0fc19faac2bbf470d3a7ed4
theleand3000 <theleand3000@gmail.com>
2020-11-02 22:12:40 -0500

    Few scripts for the prefabrcs.

---
+++ Scripts/CoroutineSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineSceneController : MonoBehaviour
+{
+    public List<Shape> gameShapes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+        }
+    }
+
+    private void SetShapesRed(){
+        foreach(Shape shape in gameShapes){
+            shape.SetColor(Color.red);
+        }
+    }
+}
